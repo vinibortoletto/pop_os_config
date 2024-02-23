0077b3b8c21fc64d15058e683afaf4f0736700c9
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2024-02-23 10:48:41 -0300

    docker-compose installation script

--- 2_docker.sh
+++ 2_docker.sh
@@ -24,12 +24,3 @@ sudo usermod -aG docker $USER
 newgrp docker
 sudo systemctl start docker
 sudo systemctl enable docker
-
-echo_doing 'Installing Docker Compose'
-
-sudo curl -L "https://github.com/docker/compose/releases/download/v2.5.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-
-echo_done
-
-
