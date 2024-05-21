de3fb567686254f3fe4b411efc89830a42bebe23
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2024-05-20 22:39:57 -0300

    php script

--- 1_essencial_apps.sh
+++ 1_essencial_apps.sh
@@ -92,3 +92,7 @@ flatpak install -y com.getpostman.Postman
 mkdir -p ~/.var/app/com.getpostman.Postman/config/Postman/proxy
 openssl req -subj '/C=US/CN=Postman Proxy' -new -newkey rsa:2048 -sha256 -days 365 -nodes -x509 -keyout ~/.var/app/com.getpostman.Postman/config/Postman/proxy/postman-proxy-ca.key -out ~/.var/app/com.getpostman.Postman/config/Postman/proxy/postman-proxy-ca.crt
 echo_done
+
+echo_doing 'Configuring PHP'
+sudo nala install -y php php-curl composer
+echo_done
