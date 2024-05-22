28cb3dc4115766aebdb8602f5d252ff5a22149f3
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2024-05-21 21:30:16 -0300

    add maven to install script

--- 8_java.sh
+++ 8_java.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 source ./.functions.sh
 
+echo_doing 'Installing Maven'
+sudo nala install maven -y
+echo_done
+
 echo_doing 'Installing JDK'
 sudo nala install default-jdk -y
 echo_done
